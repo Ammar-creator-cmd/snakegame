21d96f6eed8be7b7fe18ac0d9d6369cb05449be0
Ammar-creator-cmd <rokhimalusidr03@gmail.com>
2025-10-11 06:46:19 +0200

    Add weight conversion functionality with GUI elements

--- weight_conversion.py
+++ weight_conversion.py
@@ -6,3 +6,23 @@ app.title("Weight Conversion Tool")
 app.geometry("500x250")
 3#set bg color
 app.config(bg="lightblue")
+
+#add a  label
+label = tk.Label(app, text="Enter weight", bg="lightblue", fg="black",)
+label.config(font=("Arial", 18))
+label.pack(pady=5)
+
+#create entry box
+entry_weight = tk.Entry(app, bg ="white", fg="black")
+entry_weight.config(font=("Arial", 18))
+entry_weight.pack(pady=5)
+
+#function to convert weight
+unit_choice = tk.StringVar(app)
+unit_choice.set("Select Unit")
+unit_options = ["Kilograms to Pounds", "Pounds to Kilograms", "Kilograms to Ounces", "Ounces to Kilograms", "Pounds to Ounces", "Ounces to Pounds", "Kilograms to Grams", "Grams to Kilograms", "Pounds to Grams", "Grams to Pounds", "Ounces to Grams", "Grams to Ounces"]
+drop = tk.OptionMenu(app, unit_choice, *unit_options)
+drop.pack(pady=5)
+
+app.mainloop
+
